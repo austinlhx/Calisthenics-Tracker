20a6fe21020160185fa70415db2499c5d84f37b4
Austin Ho <austin@Austins-MacBook-Pro.local>
2020-11-14 16:43:40 -0500

    Google Pub/Sub

---
+++ main.py
@@ -0,0 +1,48 @@
+from google.cloud import pubsub_v1
+from concurrent.futures import TimeoutError
+
+# TODO(developer)
+project_id = "calistrack"
+topic_id = "CalisTrackML"
+
+publisher = pubsub_v1.PublisherClient()
+# The `topic_path` method creates a fully qualified identifier
+# in the form `projects/{project_id}/topics/{topic_id}`
+topic_path = publisher.topic_path(project_id, topic_id)
+
+for n in range(1, 10):
+    data = "Message number {}".format(n)
+    # Data must be a bytestring
+    data = data.encode("utf-8")
+    # When you publish a message, the client returns a future.
+    future = publisher.publish(topic_path, data)
+    print(future.result())
+
+print(f"Published messages to {topic_path}.")
+
+# TODO(developer)
+# project_id = "your-project-id"
+subscription_id = "CalisTrackML"
+# Number of seconds the subscriber should listen for messages
+# timeout = 5.0
+
+subscriber = pubsub_v1.SubscriberClient()
+# The `subscription_path` method creates a fully qualified identifier
+# in the form `projects/{project_id}/subscriptions/{subscription_id}`
+subscription_path = subscriber.subscription_path(project_id, subscription_id)
+
+def callback(message):
+    print(f"Received {message}.")
+    message.ack()
+
+streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+print(f"Listening for messages on {subscription_path}..\n")
+
+# Wrap subscriber in a 'with' block to automatically call close() when done.
+with subscriber:
+    try:
+        # When `timeout` is not set, result() will block indefinitely,
+        # unless an exception is encountered first.
+        streaming_pull_future.result()
+    except TimeoutError:
+        streaming_pull_future.cancel()
